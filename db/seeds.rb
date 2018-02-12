4e599ab3166bf9dbda9f8b545f106f165fc5fc66
churchwerks <theBoss@stevenlennis.com>
2018-02-12 17:13:25 -0600

    Dropped and created db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,30 +1,39 @@
 landmarks_list = {
     "Madison Square Guarden" => {
-      :year_completed => 1901
+      :year_completed => 1901,
+      :figure_id => 1
     },
     "The big bull outside" => {
-      :year_completed => 1995
+      :year_completed => 1995,
+      :figure_id => 2
     },
     "Flatiron School" => {
-      :year_completed => 2014
+      :year_completed => 2014,
+      :figure_id => 3
     },
     "Museum Mile" => {
-      :year_completed => 1058
+      :year_completed => 1958,
+      :figure_id => 4
     },
     "Subway Station" => {
-      :year_completed => 2015
+      :year_completed => 2015,
+      :figure_id => 5
     },
     "Cast Iron" => {
-      :year_completed => 1951
+      :year_completed => 1951,
+      :figure_id => 6
     },
     "Shea Stadium" => {
-      :year_completed => 1964
+      :year_completed => 1964,
+      :figure_id => 7
     },
     "Flatiron Building" => {
-      :year_completed => 1902
+      :year_completed => 1902,
+      :figure_id => 8
     },
     "Brooklyn Bridge" => {
-      :year_completed => 1883
+      :year_completed => 1883,
+      :figure_id => 9
     }
   }
 
@@ -42,10 +51,27 @@ figure_list = {
     },
     "Mark Zuckerberg" => {
     },
-    "Ada Lovelace" => {
+    "Linda Lovelace" => {
     },
-    "Linus Torvalds" => { 
+    "Linus Torvalds" => {
+    },
+    "Boss Tweed" => {
+    },
+    "Ed Koch" => {
+    },
+    "Jimmy Walker" => {
+    },
+    "Al Smith" => {
+    },
+    "Robert Moses" => {
+    },
+    "Peter Stuyvesant" => {
+    },
+    "Belle Moskowitz" => {
+    },
+    "Fiorello LaGuardia" => {
     }
+
   }
 
 figure_list.each do |name, figure_hash|
@@ -61,6 +87,12 @@ title_list = {
     "The Don" => {
     },
     "Mr. President" => {
+    },
+    "The Boss" => {
+    },
+    "The Godfather" => {
+    },
+    "Mr. Speaker" => {
     }
   }
 
